885bd222f8bbb7e83ee9f11a3c2915e9dbd90e69
Felipe Luccia de Lima <f.lipeluccia@gmail.com>
2024-12-03 15:59:21 -0300

    ProAcademy Spring Boot Project

---
+++ src/main/java/com/proacademy/ProAcademy/ProAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.proacademy.ProAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proacademy/ProAcademy/ProAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proacademy.ProAcademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
