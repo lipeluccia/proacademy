d860716d395cf64be8359ecb0e01137ae2cafa46
Felipe Luccia de Lima <f.lipeluccia@gmail.com>
2025-03-24 14:45:44 -0300

    Create UserSpringSecurity

---
+++ src/main/java/com/proacademy/ProAcademy/security/UserSpringSecurity.java
@@ -0,0 +1,62 @@
+package com.proacademy.proacademy.security;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.proacademy.proacademy.models.enums.ProfileEnum;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class UserSpringSecurity implements UserDetails {
+        
+    private Long id;
+    private String email;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserSpringSecurity(Long id, String email, String password, Set<ProfileEnum> profileEnums) {
+        super();
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.authorities = profileEnums.stream().map(x -> new SimpleGrantedAuthority(x.getDescription())).collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    
+    @Override
+    public boolean isEnabled(){
+        return true;
+    }
+
+    @Override
+    public String getUsername() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUsername'");
+    }
+
+    public boolean hasRole(ProfileEnum profileEnum) {
+        return getAuthorities().contains(new SimpleGrantedAuthority(profileEnum.getDescription()));
+    }
+}
